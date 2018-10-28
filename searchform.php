70bf7a36e605520680fc2d702747434974a985fb
bkRahul <rahulsatya1068@gmail.com>
2018-10-28 15:42:01 +0530

    a custom search form to be displayed

---
+++ searchform.php
@@ -0,0 +1,5 @@
+    <form class="form-inline my-2 my-lg-0" role="search" method="GET" action="<?php echo get_home_url('/'); ?>">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" value="<?php get_search_query(); ?>" name="s"  ar ia-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+ 
