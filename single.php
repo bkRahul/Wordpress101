c0e5f8da7c6170b12ef565bdcdb73aa521ed2d37
bkRahul <rahulsatya1068@gmail.com>
2018-10-28 15:38:32 +0530

    A default single post format

---
+++ single.php
@@ -0,0 +1,47 @@
+ 
+<?php get_header(); ?>
+
+<div class="row">
+
+<div class="col-sm-12 col-md-8">
+
+	<div class="col-xs-12">
+<?php	if(have_posts()):
+
+		while(have_posts()) : the_post(); 
+	 the_title( sprintf('<h2 class="entry-title"><a href="%s">', esc_url(get_permalink() )), ' </a></h2>'); ?>
+
+
+
+	<p><small>Posted on <?php the_time(); ?> in <?php the_category(' '); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small></p>
+ 
+	</div>
+
+	<div class="row">
+
+	<div class="col-xs-12 col-md-8">
+
+
+	 <?php the_content(); ?> 
+
+
+</div>
+
+	<div class="col-xs-12 col-md-4" style="text-align: center;">
+	
+		<div class="post-thumbnail"><?php the_post_thumbnail('medium'); ?></div>
+ <?php 	endwhile; ?>
+
+
+<?php	endif;  ?>
+	</div>
+</div> 
+<div style="text-align: center;"><?php previous_post_link(); ?> &nbsp; <?php next_post_link(); ?></div>
+
+</div>
+
+<div class="col-sm-12 col-md-4">
+	<?php get_sidebar(); ?>
+</div>
+</div>
+<?php get_footer(); ?>  
