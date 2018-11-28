67a6da0ce3a41f0439d1f882334ac93ed93705c5
bkRahul <rahulsatya1068@gmail.com>
2018-11-28 11:00:28 +0530

    Added a custom 404page

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div id="primary" class="container">
+	<main id="main" class="site-main" role="main">
+		<section class="404-error page-not-found">
+			<header>
+				<h1 class="page-title text-center">Page Not Found</h1>
+			</header>
+		</section>
+	</main>
+</div>
+
+
+<style type="text/css">
+	.error404 {
+	background: url(http://localhost/wordpress101/wp-content/uploads/2018/11/404.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+	}
+	.error404 
+
+		.bg-light {
+		background: -webkit-gradient(linear, left top, right top, from(#404d67), to(#000102));
+	}
+	.error404 .page-title {
+		color: #ebecef; 
+	} 
+</style>
+<?php get_footer(); ?>
