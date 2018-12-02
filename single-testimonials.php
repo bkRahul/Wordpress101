a8b41aa88f770c58c17065eae102d34986e899f6
bkRahul <rahulsatya1068@gmail.com>
2018-12-02 23:49:59 +0530

    Added a custom page template for testimonial

---
+++ single-testimonials.php
@@ -0,0 +1,48 @@
+ 
+<?php get_header(); ?>
+
+<div class="row">
+
+<div class="col-sm-12 col-md-8">
+
+	<div class="col-xs-12 text-center">
+<?php	if(have_posts()):
+
+		while(have_posts()) : the_post(); 
+	 the_title( sprintf('<h2 class="entry-title"><a href="%s">', esc_url(get_permalink() )), ' </a></h2>'); ?>
+
+
+
+	<p><small>Posted on <?php the_time(); ?> in <?php the_category(' '); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small></p>
+ 
+	</div>
+
+	<div class="row">
+
+	<div class="col-xs-12 col-md-12 center" style="text-align: center;">
+	
+		<div class="post-thumbnail testimonial-img"><?php the_post_thumbnail('medium'); ?></div>
+
+	</div>
+	
+	<div class="col-xs-12 col-md-12 center">
+
+
+	 <?php the_content(''); ?>
+
+ <?php 	endwhile; ?>
+
+<?php	endif;  ?>
+
+</div>
+
+</div> 
+<div style="text-align: center;"><?php previous_post_link(); ?> &nbsp; <?php next_post_link(); ?></div>
+
+</div>
+
+<div class="col-sm-12 col-md-4">
+	<?php get_sidebar(); ?>
+</div>
+</div>
+<?php get_footer(); ?>  
