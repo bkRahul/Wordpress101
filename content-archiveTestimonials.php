5fba6666aa3175824a22183d116ad5539054f4fe
bkRahul <rahulsatya1068@gmail.com>
2018-12-02 23:47:30 +0530

    Created a custom content type for testimonial archive

---
+++ content-archiveTestimonials.php
@@ -0,0 +1,34 @@
+<article id='post-<?php the_ID(); ?>' <?php post_class(); ?>>
+
+
+<div class="row">
+
+
+
+
+	<div class="col-xs-12 col-sm-4" style="text-align: center;">
+	
+		<div class="post-thumbnail testimonial-img"><?php the_post_thumbnail('thumbnail'); ?></div>
+ 
+	</div>
+
+	<div class="col-xs-12 col-sm-8" style="text-align: justify;">
+
+	<div class="col-xs-12 col-sm-12">
+
+	<?php the_title( sprintf('<h2 class="entry-title"><a href="%s">', esc_url(get_permalink() )), '</a></h2>'); ?>
+
+	</div>
+
+	<p><small>Posted on <?php the_time(); ?> in <?php the_category(' '); ?></small></p>
+
+	<p><?php the_excerpt(); ?></p>
+
+	
+	</div>
+
+</div>
+
+</article>
+
+ 	<hr/>
