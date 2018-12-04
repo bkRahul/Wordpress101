20d75a5ef5fa45b667cb93ae303b6da9c54ee966
bkRahul <rahulsatya1068@gmail.com>
2018-12-04 10:27:44 +0530

    added a custom non hierarchical taxonomy 'Services'

--- functions.php
+++ functions.php
@@ -186,17 +186,17 @@ function wordpress101_custom_taxonomies() {
 	
 	//add new taxonomy hierarchical
 	$labels = array(
-		'name' => 'Types',
-		'singular_name' => 'Type',
-		'search_items' => 'Search Types',
-		'all_items' => 'All Types',
-		'parent_item' => 'Parent Type',
-		'parent_item_colon' => 'Parent Type:',
-		'edit_item' => 'Edit Type',
-		'update_item' => 'Update Type',
-		'add_new_item' => 'Add New Testimonial Type',
-		'new_item_name' => 'New Type Name',
-		'menu_name' => 'Types'
+		'name' => 'Fields',
+		'singular_name' => 'Field',
+		'search_items' => 'Search Fields',
+		'all_items' => 'All Fields',
+		'parent_item' => 'Parent Field',
+		'parent_item_colon' => 'Parent Field:',
+		'edit_item' => 'Edit Field',
+		'update_item' => 'Update Field',
+		'add_new_item' => 'Add New Testimonial Field',
+		'new_item_name' => 'New Field Name',
+		'menu_name' => 'Fields'
 	);
 	
 	$args = array(
@@ -205,17 +205,19 @@ function wordpress101_custom_taxonomies() {
 		'show_ui' => true,
 		'show_admin_column' => true,
 		'query_var' => true,
-		'rewrite' => array( 'slug' => 'type' )
+		'rewrite' => array( 'slug' => 'field' )
 	);
 	
-	register_taxonomy('type', array('testimonials'), $args);
+	register_taxonomy('field', array('testimonials'), $args);
 	
 	//add new taxonomy NOT hierarchical
 	
+	register_taxonomy('services', 'testimonials', array(
+		'label' => 'Services',
+		'rewrite' => array( 'slug' => 'services' ),
+		'hierarchical' => false
+	));
 }
 add_action( 'init' , 'wordpress101_custom_taxonomies' );
 
-
 ?>
-
-
